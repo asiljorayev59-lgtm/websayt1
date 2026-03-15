88cd64858ff6a78d54f137578fcc6289dd66bbdf
asiljorayev59-lgtm <asiljorayev59@gmail.com>
2026-03-15 08:28:04 -0300

    Refactor server.js to improve code structure

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const express=require("express")
-const fs=require("fs")
+const express = require("express")
+const fs = require("fs")
 
-const app=express()
+const app = express()
 
 app.use(express.static("."))
 
 app.get("/signals",(req,res)=>{
 
-const data=fs.readFileSync("./data/signals.json")
+const data = fs.readFileSync("data/signals.json")
 
 res.json(JSON.parse(data))
 
 })
 
-app.listen(3000)
+app.get("/history",(req,res)=>{
+
+const data = fs.readFileSync("data/history.json")
+
+res.json(JSON.parse(data))
+
+})
+
+app.listen(3000,()=>{
+console.log("Server running")
+})
